792a040894de0344625aa20d0e95583a4cc054dc
{junshun} <{3444374315@qq.com}>
2026-09-02 17:25:34 +0800

    fix: require extension membership for semantic markers

--- code/postgres/semloom_pg/src/extension.c
+++ code/postgres/semloom_pg/src/extension.c
@@ -1,6 +1,9 @@
 #include "postgres.h"
 
+#include "catalog/dependency.h"
+#include "catalog/pg_proc_d.h"
 #include "catalog/pg_type_d.h"
+#include "commands/extension.h"
 #include "fmgr.h"
 #include "optimizer/paths.h"
 #include "optimizer/planner.h"
@@ -37,31 +40,49 @@ static void semloom_set_rel_pathlist(PlannerInfo *root,
 void _PG_init(void);
 void _PG_fini(void);
 
+static Oid
+semloom_lookup_marker(const char *name, int nargs, const Oid *argument_types)
+{
+	Oid extension_oid = get_extension_oid("semloom_pg", true);
+	Oid function_oid;
+	List *qualified_name;
+
+	if (!OidIsValid(extension_oid))
+		return InvalidOid;
+
+	qualified_name = list_make2(makeString(pstrdup("ai_semantic")),
+								makeString(pstrdup(name)));
+	function_oid = LookupFuncName(qualified_name, nargs, argument_types, true);
+	list_free_deep(qualified_name);
+	if (!OidIsValid(function_oid) ||
+		getExtensionOfObject(ProcedureRelationId, function_oid) != extension_oid)
+		return InvalidOid;
+
+	return function_oid;
+}
+
 Oid
 semloom_map_function_oid(void)
 {
-	List *qualified_name = list_make2(makeString("ai_semantic"), makeString("map"));
 	Oid argument_types[1] = {TEXTOID};
 
-	return LookupFuncName(qualified_name, lengthof(argument_types), argument_types, true);
+	return semloom_lookup_marker("map", lengthof(argument_types), argument_types);
 }
 
 Oid
 semloom_filter_function_oid(void)
 {
-	List *qualified_name = list_make2(makeString("ai_semantic"), makeString("filter"));
 	Oid argument_types[1] = {TEXTOID};
 
-	return LookupFuncName(qualified_name, lengthof(argument_types), argument_types, true);
+	return semloom_lookup_marker("filter", lengthof(argument_types), argument_types);
 }
 
 Oid
 semloom_exact_filter_function_oid(void)
 {
-	List *qualified_name = list_make2(makeString("ai_semantic"), makeString("filter"));
 	Oid argument_types[3] = {TEXTOID, TEXTOID, JSONBOID};
 
-	return LookupFuncName(qualified_name, lengthof(argument_types), argument_types, true);
+	return semloom_lookup_marker("filter", lengthof(argument_types), argument_types);
 }
 
 const char *
